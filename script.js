488926acac94a4b0417fe429389b52f2eb221f93
Mohab <mmss.em@gmail.com>
2015-10-15 21:26:15 +0400

    generating cards from template

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function ($) {
-	$('textarea.adjheight').keyup(function() {
+	$(document).delegate('textarea.adjheight', 'keyup', function() {
 		defautlHeight = 90;
 		this.style.height = defautlHeight + 'px';
 		this.style.height = (this.scrollHeight) + 'px';
@@ -21,22 +21,22 @@
 		socialmedia: [],
 		imagetypes: []
 	};
-	function genChannels(channels) {
+	function tempChannels(channels) {
 		var temp = _.template($('#channels_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
 		$('#channels').html(temp({channels: channels}));
 		$('#channels').show();
 		$('#channels select').change(function() {
 			templatesVars.channel = $('#channels option:selected').val();
 			if (templatesVars.channel.length) {
-				genLangs(channelsMap.channels[templatesVars.channel].langs);
-				genSocialmedia(channelsMap.channels[templatesVars.channel].socialmedia);
+				tempLangs(channelsMap.channels[templatesVars.channel].langs);
+				tempSocialmedia(channelsMap.channels[templatesVars.channel].socialmedia);
 			} else {
-				genLangs([]);
-				genSocialmedia([]);
+				tempLangs([]);
+				tempSocialmedia([]);
 			}
 		});
 	}
-	function genLangs(langs) {
+	function tempLangs(langs) {
 		if (langs.length == 0) {
 			$('#langs input').prop('checked', false).trigger('change');
 			$('#langs').hide().html('');
@@ -59,7 +59,7 @@
 		});
 		repropagateCheckboxes('langs');
 	}
-	function genSocialmedia(socialmedia) {
+	function tempSocialmedia(socialmedia) {
 		if (socialmedia.length == 0) {
 			$('#socialmedia input').prop('checked', false).trigger('change');
 			$('#socialmedia').hide().html('');
@@ -67,7 +67,7 @@
 		}
 		_socialmedia = [];
 		socialmedia.forEach(function(sm) {
-			_socialmedia.push([sm, channelsMap.socialmedia[sm].name]);
+			_socialmedia.push([sm, channelsMap.socialmedia[sm].title]);
 		});
 		var temp = _.template($('#socialmedia_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
 		$('#socialmedia').html(temp({socialmedia: _socialmedia}));
@@ -83,11 +83,11 @@
 			templatesVars.socialmedia.forEach(function (sm) {
 				imagetypes.push.apply(imagetypes, Object.keys(channelsMap.socialmedia[sm].imagetypes))
 			});
-			genImagetypes($.unique(imagetypes));
+			tempImagetypes($.unique(imagetypes));
 		});
 		repropagateCheckboxes('socialmedia');
 	}
-	function genImagetypes(imagetypes) {
+	function tempImagetypes(imagetypes) {
 		if (imagetypes.length == 0) {
 			$('#imagetype input').prop('checked', false).trigger('change');
 			$('#imagetype').hide().html('');
@@ -95,7 +95,7 @@
 		}
 		_imagetypes = [];
 		imagetypes.forEach(function(it) {
-			_imagetypes.push([it, channelsMap.imagetypes[it].value, channelsMap.imagetypes[it].title]);
+			_imagetypes.push([it, channelsMap.imagetypes[it].title]);
 		});
 		var temp = _.template($('#imagetypes_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
 		$('#imagetype').html(temp({imagetypes: _imagetypes}));
@@ -113,6 +113,15 @@
 	$('.requirements').delegate('input, select', 'change', function() {
 		$('button#gencards').prop('disabled', !(Object.keys(templatesVars).every(function(key) {return templatesVars[key].length})));
 	});
+	function tempCard(cardData) {
+		if (cardData.length == 0) {
+			$('#cards').hide().html('');
+			return false;
+		}
+		var temp = _.template($('#card_temp').html().replace(/&lt;/g, '<').replace(/&gt;/g, '>'));
+		$('#cards').html($('#cards').html() + temp({data: cardData}));
+		$('#cards').show();
+	}
 	// - Template generators
 	// ---------------------
 
@@ -120,10 +129,10 @@
 	$(document).ready(function () {
 		$.ajax("map.json", {
 			"dataType": "json",
-			"success": function (map) {
+			"success": function(map) {
 				// Building requirements form
 				channelsMap = map;
-				genChannels(Object.keys(map.channels));
+				tempChannels(Object.keys(map.channels));
 			}
 		})
 	});
@@ -134,6 +143,8 @@
 		if (cardsGenerated) {
 			if (!window.confirm('The currently generated cards will be lost.  Are you sure you want to continue generating new cards?')) {
 				return false;
+			} else {
+				tempCards([]);
 			}
 		}
 
@@ -147,26 +158,33 @@
 				data[item.name].push(item.value);
 			}
 		});
-		console.log(data);
 
-		// verifying form data
-		// var verified = true;
-		// var requiredData = [{'key':'channel','value':'a channel'},
-		// 					{'key':'socialmedia','value':'a social media'},
-		// 					{'key':'imagetype','value':'an image type'},
-		// 					{'key':'language','value':'a language'}]
-		// requiredData.forEach(function(item) {
-		// 	if (verified && !data.hasOwnProperty(item.key)) {
-		// 		alert('Please choose ' + item.value + ' from the options');
-		// 		verified = false;
-		// 	}
-		// });
-		// if (!verified) return false;
-
-		// generating cards variables
-		cardsVars = [];
-		requiredData.forEach(function(item) {
-
+		// generating cards
+		cardsData = [];
+		for (i1 = 0; i1 < data.channels.length; i1++) {
+			for (i2 = 0; i2 < data.langs.length; i2++) {
+				if (channelsMap.channels[data.channels[i1]].langs.indexOf(data.langs[i2]) == -1) break;
+				for (i3 = 0; i3 < data.socialmedia.length; i3++) {
+					if (channelsMap.channels[data.channels[i1]].socialmedia.indexOf(data.socialmedia[i3]) == -1) break;
+					for (i4 = 0; i4 < data.imagetypes.length; i4++) {
+						if (!channelsMap.socialmedia[data.socialmedia[i3]].imagetypes.hasOwnProperty(data.imagetypes[i4])) break;
+						cardsData.push({
+							'requestby': data.requestby[0],
+							'requestto': data.requestto[0],
+							'date': data.date[0],
+							'deadline': data.deadline[0],
+							'channel': data.channels[i1],
+							'lang': channelsMap.langs[data.langs[i2]],
+							'socialmedia': channelsMap.socialmedia[data.socialmedia[i3]].title,
+							'imagetype': channelsMap.imagetypes[data.imagetypes[i4]].value,
+							'resolution': channelsMap.socialmedia[data.socialmedia[i3]].imagetypes[data.imagetypes[i4]]
+						});
+					}
+				}
+			}
+		}
+		cardsData.forEach(function(card) {
+			tempCard(card);
 		});
 
 		// cardsGenerated = true;
